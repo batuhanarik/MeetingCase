38b3785e90bc426fd37089d4ef55d377ab3f887f
Batuhan Arık <batuhanarik123@gmail.com>
2024-03-06 01:53:54 +0300

    IEntityRepository interface writed

---
+++ Business/Abstract/IUserDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+        //List<OperationClaim> GetClaims(User user);
+    }
+}
