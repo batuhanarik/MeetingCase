8ced9bd229f1f300cf6a6a062bdfe851b5e7a725
Batuhan Arık <batuhanarik123@gmail.com>
2024-03-11 05:24:02 +0300

    last changes

---
+++ DataAccess/Migrations/20240311020616_latest-mig.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class latestmig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+            table: "OperationClaims",
+            columns: new[] { "Id", "Name" },
+            values: new object[,]
+            {
+                { 1,"superadmin" },
+                { 2,"admin" },
+                { 3,"product_manager" },
+                { 4,"developer" },
+                { 5,"intern" },
+            });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
